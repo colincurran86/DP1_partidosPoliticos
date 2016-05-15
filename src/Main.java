b110757cd48b65cfee6557a9ffcce39ff1fa0445
tdachris <donchristian93@gmail.com>
2016-05-15 17:49:27 -0500

    Holi

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Larga Vida a Christian Dongo ");
-		System.out.println("Lof foreva");
-
+		System.out.println("Larga Vida a Christian Dongo:):):):):):))");
 	}
 
 }
