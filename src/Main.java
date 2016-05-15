2e9578efad586d33c1750ad78ba99f4551d78bbb
tdachris <donchristian93@gmail.com>
2016-05-15 17:54:19 -0500

    Jose es lo máximo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Jose es lo máximo! 
 		System.out.println("Larga Vida a Christian Dongo:):):):):):))");
 	
 		System.out.println("Lofe Foreva  ");
