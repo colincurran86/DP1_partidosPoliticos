c87bd539d4f1f69b36a9fbf37b05a4582c91e7fd
Jose <jemarroquin@pucp.pe>
2016-05-15 17:51:33 -0500

    Lofiuu

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Larga Vida a Christian Dongo:):):):):):))");
+	
+		System.out.println("Lofe Foreva  ");
+
 	}
 
 }
