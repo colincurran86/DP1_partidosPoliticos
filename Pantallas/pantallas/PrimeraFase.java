a21e21affb4be63eb9eaf5327d54685c481967db
tdachris <donchristian_7@hotmail.com>
2016-06-06 11:09:54 -0500

    select folder

--- Pantallas/pantallas/PrimeraFase.java
+++ Pantallas/pantallas/PrimeraFase.java
@@ -30,11 +30,11 @@ public class PrimeraFase extends JPanel implements ActionListener {
 		setLayout(null);
 
 		JLabel lblRutaDeLos = new JLabel("Ruta de los planillones");
-		lblRutaDeLos.setBounds(90, 177, 126, 14);
+		lblRutaDeLos.setBounds(90, 177, 215, 14);
 		add(lblRutaDeLos);
 
 		JLabel lblRutaDeLa = new JLabel("Ruta de la Base de Datos");
-		lblRutaDeLa.setBounds(90, 308, 126, 14);
+		lblRutaDeLa.setBounds(90, 308, 228, 14);
 		add(lblRutaDeLa);
 
 		btnProcesar = new JButton("Procesar");
@@ -84,9 +84,11 @@ public class PrimeraFase extends JPanel implements ActionListener {
 		}
 		if (e.getSource() == btnBuscarBD) {
 			JFileChooser jFileChooser = new JFileChooser();
+			jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			jFileChooser.setAcceptAllFileFilterUsed(false);
+			
 			int result = jFileChooser.showOpenDialog(this);
-			StringBuffer buffer = new StringBuffer();		
-			//jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			StringBuffer buffer = new StringBuffer();					
 
 			if (result == JFileChooser.APPROVE_OPTION) {// abrir				
 				File file = jFileChooser.getSelectedFile();
@@ -96,12 +98,18 @@ public class PrimeraFase extends JPanel implements ActionListener {
 		}
 		if (e.getSource() == btnBuscarPlan){
 			JFileChooser jFileChooser = new JFileChooser();
+			jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			jFileChooser.setAcceptAllFileFilterUsed(false);
+			
 			int result = jFileChooser.showOpenDialog(this);
 			StringBuffer buffer = new StringBuffer();			
-			//jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			
 			
 			if (result == JFileChooser.APPROVE_OPTION) {// abrir				
 				File file = jFileChooser.getSelectedFile();
+				/*if (!file.isDirectory()) 
+			        file = file.getParentFile();
+			        file= file.getCurrentDirectory();*/			    
 				txtFieldPlan.setText(file.getAbsolutePath());
 			}
 		}
